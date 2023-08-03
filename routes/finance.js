c50e5cd77a7c4431b2f50e5889fa2307598b0126
SigmaSix6 <SigmaSix6@users.noreply.github.com>
2023-08-03 16:05:08 +0700

    Push Before Demo

--- routes/finance.js
+++ routes/finance.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { con } = require("../configs/connection");
 
 const convertDateToSql = (data) => {
-  data = data.toISOString().slice(0, 19).replace("T", " ");
+  return data.slice(0, 19).replace("T", " ");
 };
 
 router.get("/", function (req, res, next) {
@@ -74,25 +74,36 @@ router.get("/element", function (req, res, next) {
 });
 
 router.post("/insert-payment-req", async (req, res, next) => {
-  // console.log(req.body);
-  let res1;
-  await con
-    .execute(
-      "INSERT INTO `payment_requisition` (`PR_numb`, `PR_supp_name`, `PR_supp_number`, `PR_dept_numb`, `PR_req_date`, `PR_type_pay`, `PR_mode_pay`, `PR_proj_numb`, `PR_PO_numb`, `PR_amt_idr_tot`, `PR_amt_usd_tot`, `PR_tax_usd_tot`, `PR_tax_idr_tot`, `PR_effec_date`, `PR_alloc_dept`, `PR_ele_req`, `PR_finc_issue_date`, `PR_finc_recv_inv`, `PR_supp_inv_numb`, `PR_finc_bank_doc`, `PR_finc_bank_n_b`, `PR_finc_bank_n_g`, `PR_finc_date_paid`, `PR_amt_tsf_fee`, `PR_amt_tot_all_idr`, `Comp_code_pr`) VALUES (NULL, 'aaa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL); ",
-      (err, rows, fields) => {
-        if (err) throw err;
-        console.log("Success");
-        res1 = rows;
-      }
-    )
-    .then(
-      async (success) => {
-        await con.execute(
-          `INSERT INTO 'payment_requisition_detail' ('id', 'pr_req_desc', 'pr_amt_idr', 'pr_amt_usd', 'pr_tax', 'pr_tax_usd', 'pr_tax_idr', 'pr_amt_curr', 'pr_type_curr', 'pr_amt_tot_idr') VALUES ('${res1.insertId}', 'ASD', '', '', '', '', '', '', '', '')`
+  const data = req.body;
+  // data.request_date = convertDateToSql(data.request_date);
+  await con.execute(
+    `INSERT INTO payment_requisition (PR_numb, PR_supp_name, PR_supp_number, PR_dept_numb, PR_req_date, PR_type_pay, PR_mode_pay, PR_proj_numb, PR_PO_numb, PR_amt_idr_tot, PR_amt_usd_tot, PR_tax_usd_tot, PR_tax_idr_tot, PR_effec_date, PR_alloc_dept, PR_ele_req, PR_finc_issue_date, PR_finc_recv_inv, PR_supp_inv_numb, PR_finc_bank_doc, PR_finc_bank_n_b, PR_finc_bank_n_g, PR_finc_date_paid, PR_amt_tsf_fee, PR_amt_tot_all_idr, Comp_code_pr) VALUES (NULL, '${data.supplier_name}', '${data.supplier_number}', '${data.department_number}', '${data.request_date}', '${data.type_payment}', '${data.mode_payment}', '${data.project_number}', '${data.po_number}', '${data.total_idr}', '${data.total_currency}', '${data.tax_idr}', '${data.tax_currency}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL); `,
+    (err, rows, fields) => {
+      if (err) throw err;
+      // console.log("Success");
+      for (let i = 1; i <= data.detailLength; i++) {
+        con.execute(
+          `INSERT INTO payment_requisition_detail (id, pr_req_desc, pr_amt_idr, pr_amt_usd, pr_tax, pr_tax_usd, pr_tax_idr, pr_amt_curr, pr_type_curr, pr_amt_tot_idr) VALUES ('${
+            rows.insertId
+          }', '${data[`purpose_req_${i}`] ?? ``}', '${data[`amount_${i}`] ?? ``}', '${data[`curr_${i}`] ?? ``}', '${data[`tax_${i}`] ?? ``}', '${
+            rows.insertId
+          }', '${rows.insertId}', '${rows.insertId}', '${rows.insertId}', '${rows.insertId}') `,
+          (err, rows, fields) => {
+            if (err) throw err;
+            console.log(rows);
+          }
         );
-      },
-      async (reject) => {}
-    );
+      }
+    }
+  );
+  // .then(
+  //   async (success) => {
+  //     await con.execute(
+  //       `INSERT INTO 'payment_requisition_detail' ('id', 'pr_req_desc', 'pr_amt_idr', 'pr_amt_usd', 'pr_tax', 'pr_tax_usd', 'pr_tax_idr', 'pr_amt_curr', 'pr_type_curr', 'pr_amt_tot_idr') VALUES ('${res1.insertId}', 'ASD', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)`
+  //     );
+  //   },
+  //   async (reject) => {}
+  // );
 });
 
 module.exports = router;

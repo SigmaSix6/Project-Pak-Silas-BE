fbbb64de28edc0fbf572f00454e064d2ede881d5
SigmaSix6 <SigmaSix6@users.noreply.github.com>
2023-10-09 21:53:53 +0700

    Cash Advance Progress

--- routes/finance.js
+++ routes/finance.js
@@ -35,7 +35,7 @@ router.get("/bank-name", function (req, res, next) {
   con.execute("SELECT * FROM bank_name ORDER BY `bank_name` ASC", (err, rows, fields) => {
     if (err) throw err;
     console.log(rows);
-    res.send(rows);
+    return true;
   });
 });
 
@@ -85,6 +85,7 @@ router.post("/insert-payment-req", async (req, res, next) => {
           (err, rows, fields) => {
             if (err) throw err;
             console.log(rows);
+            return true;
           }
         );
       }
@@ -96,7 +97,7 @@ router.post("/insert-cash-adv", async (req, res, next) => {
   const data = req.body;
   // data.request_date = convertDateToSql(data.request_date);
   await con.execute(
-    `INSERT INTO cash_advance(CA_ID_Numb, CA_supp_name, CA_supp_numb, CA_dept_numb, CA_proj_numb, CA_req_date, CA_type_pay, CA_mode_pay, CA_req_desc, CA_effec_date, CA_cheque_no, CA_amt_usd, CA_amt_idr, CA_amt_say, CA_finance_prog_date, CA_finance_notice, CA_alloc_dept, CA_element, CA_balance_amt, CA_balance_amt_curr, CA_return_to_cash, CA_return_to_cash_curr, CA_date_in_site, CA_date_out_site, CA_total_empl_on_duty, CA_P_amt_ticket, CA_P_amt_airport_tax, CA_P_amt_taxi, CA_P_amt_meal, CA_P_amt_accom, CA_P_amt_park_toll, CA_P_amt_doc, CA_P_amt_fee_comn, CA_P_amt_purch_mat, CA_P_amt_sent_mat, CA_P_amt_rental_car, CA_P_amt_fuel, CA_P_amt_telp_voucher, CA_P_amt_other1, CA_P_amt_other2, CA_P_amt_other3, CA_P_amt_other4, CA_P_amt_other5, CA_amt_ticket, CA_amt_airport_tax, CA_amt_taxi, CA_amt_meal, CA_amt_accom, CA_amt_park_toll, CA_amt_doc, CA_amt_fee_comn, CA_amt_purch_mat, CA_amt_sent_mat, CA_amt_rental_car, CA_amt_fuel, CA_amt_telp_voucher, CA_amt_other1, CA_amt_other2, CA_amt_other3, CA_amt_other4, CA_amt_other5, CA_no_amt_ticket, CA_no_amt_airport_tax, CA_no_amt_taxi, CA_no_amt_meal, CA_no_amt_accom, CA_no_amt_park_toll, CA_no_amt_doc, CA_no_amt_fee_comn, CA_no_amt_purch_mat, CA_no_amt_sent_mat, CA_no_amt_rental_car, CA_no_amt_fuel, CA_no_amt_telp_voucher, CA_no_amt_other1, CA_no_amt_other2, CA_no_amt_other3, CA_no_amt_other4, CA_no_amt_other5, P_currency, P_Curr_accom, P_Curr_ticket, P_Curr_meal, P_Curr_enter, P_Curr_taxi, P_Curr_other, A_Curr_accom, A_Curr_ticket, A_Curr_meal, A_Curr_enter, A_Curr_taxi, A_Curr_other, CA_status, Curr_amt, Comp_code_ca) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]','[value-12]','[value-13]','[value-14]','[value-15]','[value-16]','[value-17]','[value-18]','[value-19]','[value-20]','[value-21]','[value-22]','[value-23]','[value-24]','[value-25]','[value-26]','[value-27]','[value-28]','[value-29]','[value-30]','[value-31]','[value-32]','[value-33]','[value-34]','[value-35]','[value-36]','[value-37]','[value-38]','[value-39]','[value-40]','[value-41]','[value-42]','[value-43]','[value-44]','[value-45]','[value-46]','[value-47]','[value-48]','[value-49]','[value-50]','[value-51]','[value-52]','[value-53]','[value-54]','[value-55]','[value-56]','[value-57]','[value-58]','[value-59]','[value-60]','[value-61]','[value-62]','[value-63]','[value-64]','[value-65]','[value-66]','[value-67]','[value-68]','[value-69]','[value-70]','[value-71]','[value-72]','[value-73]','[value-74]','[value-75]','[value-76]','[value-77]','[value-78]','[value-79]','[value-80]','[value-81]','[value-82]','[value-83]','[value-84]','[value-85]','[value-86]','[value-87]','[value-88]','[value-89]','[value-90]','[value-91]','[value-92]','[value-93]','[value-94]','[value-95]')`,
+    `INSERT INTO cash_advance(CA_ID_Numb, CA_supp_name, CA_supp_numb, CA_dept_numb, CA_proj_numb, CA_req_date, CA_type_pay, CA_mode_pay, CA_req_desc, CA_effec_date, CA_cheque_no, CA_amt_usd, CA_amt_idr, CA_amt_say, CA_finance_prog_date, CA_finance_notice, CA_alloc_dept, CA_element, CA_balance_amt, CA_balance_amt_curr, CA_return_to_cash, CA_return_to_cash_curr, CA_date_in_site, CA_date_out_site, CA_total_empl_on_duty, CA_P_amt_ticket, CA_P_amt_airport_tax, CA_P_amt_taxi, CA_P_amt_meal, CA_P_amt_accom, CA_P_amt_park_toll, CA_P_amt_doc, CA_P_amt_fee_comn, CA_P_amt_purch_mat, CA_P_amt_sent_mat, CA_P_amt_rental_car, CA_P_amt_fuel, CA_P_amt_telp_voucher, CA_P_amt_other1, CA_P_amt_other2, CA_P_amt_other3, CA_P_amt_other4, CA_P_amt_other5, CA_amt_ticket, CA_amt_airport_tax, CA_amt_taxi, CA_amt_meal, CA_amt_accom, CA_amt_park_toll, CA_amt_doc, CA_amt_fee_comn, CA_amt_purch_mat, CA_amt_sent_mat, CA_amt_rental_car, CA_amt_fuel, CA_amt_telp_voucher, CA_amt_other1, CA_amt_other2, CA_amt_other3, CA_amt_other4, CA_amt_other5, CA_no_amt_ticket, CA_no_amt_airport_tax, CA_no_amt_taxi, CA_no_amt_meal, CA_no_amt_accom, CA_no_amt_park_toll, CA_no_amt_doc, CA_no_amt_fee_comn, CA_no_amt_purch_mat, CA_no_amt_sent_mat, CA_no_amt_rental_car, CA_no_amt_fuel, CA_no_amt_telp_voucher, CA_no_amt_other1, CA_no_amt_other2, CA_no_amt_other3, CA_no_amt_other4, CA_no_amt_other5, P_currency, P_Curr_accom, P_Curr_ticket, P_Curr_meal, P_Curr_enter, P_Curr_taxi, P_Curr_other, A_Curr_accom, A_Curr_ticket, A_Curr_meal, A_Curr_enter, A_Curr_taxi, A_Curr_other, CA_status, Curr_amt, Comp_code_ca) VALUES ('[value-1]','${data.employee_name}','${data.employee_number}','${data.department_no}','${data.project_number}','${data.requisition_date}','${data.type_payment}','${data.mode_payment}','${data.purpose_of_request}','[effec_date]','[cheque_no]','${data.total_idr_request}','${data.total_currency_request}','${data.plan_date_on_field}','${data.plan_date_out_field}','${data.total_manpower_duty}','${data.department_user}','${data.element_allocation}','balance_amount','balance_amount_curr','return_to_cash','date_in_site','date_out_site','total_empl_on_duty','${data.ticket_airplane_tram}','${data.airport_tax}','${data.taxi}','${data.meal}','${data.accomodation}','${data.park_toll}','${data.documentation}','${data.fee_comission}','${data.local_material_purchase}','${data.material_delivery}','${data.rental_car}','${data.fuel}','${data.telephone_voucher}','${data.others_1}','${data.others_2}','${data.others_3}','${data.others_4}','${data.others_5}','[value-43]','[value-44]','[value-45]','[value-46]','[value-47]','[value-48]','[value-49]','[value-50]','[value-51]','[value-52]','[value-53]','[value-54]','[value-55]','[value-56]','[value-57]','[value-58]','[value-59]','[value-60]','[value-61]','[value-62]','[value-63]','[value-64]','[value-65]','[value-66]','[value-67]','[value-68]','[value-69]','[value-70]','[value-71]','[value-72]','[value-73]','[value-74]','[value-75]','[value-76]','[value-77]','[value-78]','[value-79]','[value-80]','[value-81]','[value-82]','[value-83]','[value-84]','[value-85]','[value-86]','[value-87]','[value-88]','[value-89]','[value-90]','[value-91]','[value-92]','[value-93]','[value-94]','[value-95]')`,
     (err, rows, fields) => {
       if (err) throw err;
       // console.log("Success");
@@ -110,6 +111,7 @@ router.post("/insert-cash-adv", async (req, res, next) => {
           (err, rows, fields) => {
             if (err) throw err;
             console.log(rows);
+            return true;
           }
         );
       }
@@ -121,10 +123,11 @@ router.post("/insert-bank-activity", async (req, res, next) => {
   const data = req.body;
   // data.request_date = convertDateToSql(data.request_date);
   await con.execute(
-    `INSERT INTO bank_activity (bank_name, date_transaction, bank_remark, bank_code, bank_curr, bank_deposit_idr, bank_withdrawal_idr, bank_deposit_usd, bank_withdrawal_usd, cheque, finance_description, comp_code_ba, bank_deposit_eur, bank_withdrawal_eur) VALUES ( ${data.bank_name}, ${data.transaction_date}, ${data.remarks}, NULL, ${data.transaction_code}, ${data.currency_type}, ${data.deposit_idr}, ${data.withdrawal_idr}, ${data.deposit_usd}, ${data.withdrawal_usd}, NULL, ${data.finance_description}, NULL, NULL)`,
+    `INSERT INTO bank_activity (bank_name, date_transaction, bank_remark, bank_code, bank_curr, bank_deposit_idr, bank_withdrawal_idr, bank_deposit_usd, bank_withdrawal_usd, cheque, finance_description, comp_code_ba, bank_deposit_eur, bank_withdrawal_eur) VALUES ( '${data.bank_name}', '${data.transaction_date}', '${data.remarks}', NULL, '${data.transaction_code}', '${data.currency_type}', '${data.deposit_idr}', '${data.withdrawal_idr}', '${data.deposit_usd}', '${data.withdrawal_usd}', NULL, '${data.finance_description}', NULL, NULL)`,
     (err, rows, fields) => {
       if (err) throw err;
       console.log(rows);
+      return true;
     }
   );
 });
@@ -133,10 +136,11 @@ router.post("/insert-supplier", async (req, res, next) => {
   const data = req.body;
   // data.request_date = convertDateToSql(data.request_date);
   await con.execute(
-    `INSERT INTO supplier_data (supp_name, supp_number, supp_address, supp_contact_name, supp_phone_numb) VALUES ( ${data.supplier_name}, ${data.supplier_number}, ${data.address}, ${data.contact_name}, ${data.phone_number})`,
+    `INSERT INTO supplier_data (supp_name, supp_number, supp_address, supp_contact_name, supp_phone_numb) VALUES ( '${data.supplier_name}', '${data.supplier_number}', '${data.address}', '${data.contact_name}', '${data.phone_number}')`,
     (err, rows, fields) => {
       if (err) throw err;
       console.log(rows);
+      return true;
     }
   );
 });
@@ -144,11 +148,13 @@ router.post("/insert-supplier", async (req, res, next) => {
 router.post("/insert-payment-order", async (req, res, next) => {
   const data = req.body;
   // data.request_date = convertDateToSql(data.request_date);
+  console.log(data.supplier_name);
   await con.execute(
-    `INSERT INTO payment_po (PO_numb, PO_PO_Numb, PO_supp_name, PO_supp_numb, PO_proj_numb, PO_dept_numb, PO_req_date, PO_type_pay, PO_mode_pay, PO_type, PO_req_desc, PO_supp_inv_numb, PO_inv_rec_date, PO_amt_usd, PO_amt_idr, PO_amt_curr_t, PO_tax_y_n, PO_tax_usd, PO_tax_idr, PO_finc_effec_date, PO_finc_issue, PO_finc_amt_tsf_fee, PO_finc_amt_oth_fee, PO_amt_tot_usd, PO_amt_curr, PO_amt_tot_idr, PO_amt_all_tot_idr, PO_finc_bank_doc, PO_finc_bank_n_b, PO_finc_bank_n_g, PO_amt_all_tot_idr2, Comp_code_po) VALUES ('',${data.po_number},${data.supplier_name},${data.supplier_number},${data.project_number},${data.department_number},${data.request_date},${data.type_payment},${data.mode_payment},${data.po_type},${data.po_description},${data.supplier_inv_number},${data.inv_date_received},${data.po_amount_currency},${data.po_amount_idr},'[value-16]','[value-17]','[value-18]','[value-19]','[value-20]','[value-21]','[value-22]','[value-23]','[value-24]','[value-25]','[value-26]','[value-27]','[value-28]','[value-29]','[value-30]','[value-31]','[value-32]')`,
+    `INSERT INTO payment_po (PO_numb, PO_PO_Numb, PO_supp_name, PO_supp_numb, PO_proj_numb, PO_dept_numb, PO_req_date, PO_type_pay, PO_mode_pay, PO_type, PO_req_desc, PO_supp_inv_numb, PO_inv_rec_date, PO_amt_usd, PO_amt_idr, PO_amt_curr_t, PO_tax_y_n, PO_tax_usd, PO_tax_idr, PO_finc_effec_date, PO_finc_issue, PO_finc_amt_tsf_fee, PO_finc_amt_oth_fee, PO_amt_tot_usd, PO_amt_curr, PO_amt_tot_idr, PO_amt_all_tot_idr, PO_finc_bank_doc, PO_finc_bank_n_b, PO_finc_bank_n_g, PO_amt_all_tot_idr2, Comp_code_po) VALUES ('','${data.po_number}','${data.supplier_name}','${data.supplier_number}','${data.project_number}','${data.department_number}','${data.request_date}','${data.type_payment}','${data.mode_payment}','${data.po_type}','${data.po_description}','${data.supplier_inv_number}','${data.inv_date_received}','${data.po_amount_currency}','${data.po_amount_idr}','[value-16]','[value-17]','[value-18]','[value-19]','[value-20]','[value-21]','[value-22]','[value-23]','[value-24]','[value-25]','[value-26]','[value-27]','[value-28]','[value-29]','[value-30]','[value-31]','[value-32]')`,
     (err, rows, fields) => {
       if (err) throw err;
       console.log(rows);
+      return true;
     }
   );
 });
